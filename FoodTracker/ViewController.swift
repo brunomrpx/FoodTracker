0cb3cbc241dde577d18739d480626c7b27e8a303
Bruno Paixao <brunomrpx@gmail.com>
2016-12-06 23:20:40 -0200

    First release - finished Build a Basic UI

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Bruno Mayer Paixão on 8/13/16.
+//  Copyright © 2016 Bruno Mayer Paixão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
